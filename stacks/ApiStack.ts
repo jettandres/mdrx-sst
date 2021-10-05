f7e93d4f2394a042d67dbb2642cc52b020655e82
Jett Robin Andres <jettrobin.andres@gmail.com>
2021-10-06 00:19:39 +0800

    Rename stacks and lambda

---
+++ stacks/ApiStack.ts
@@ -0,0 +1,19 @@
+import * as sst from "@serverless-stack/resources"
+
+export default class ApiStack extends sst.Stack {
+  constructor(scope: sst.App, id: string, props?: sst.StackProps) {
+    super(scope, id, props)
+
+    // Create a HTTP API
+    const api = new sst.Api(this, "Api", {
+      routes: {
+        "POST /v1/compute-expense-report": "src/computeExpenseReport.handler",
+      },
+    })
+
+    // Show the endpoint in the output
+    this.addOutputs({
+      ApiEndpoint: `${api.url}/v1/compute-expense-report`,
+    })
+  }
+}
--- stacks/index.ts
+++ stacks/index.ts
@@ -1,13 +1,13 @@
-import MyStack from "./MyStack";
-import * as sst from "@serverless-stack/resources";
+import ApiStack from "./ApiStack"
+import * as sst from "@serverless-stack/resources"
 
 export default function main(app: sst.App): void {
   // Set default runtime for all functions
   app.setDefaultFunctionProps({
-    runtime: "nodejs12.x"
-  });
+    runtime: "nodejs12.x",
+  })
 
-  new MyStack(app, "my-stack");
+  new ApiStack(app, "api-stack")
 
   // Add more stacks
 }
