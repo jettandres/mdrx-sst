49d108c6531d0be7951714874ed770ce94aee1ce
Jett Robin Andres <jettrobin.andres@gmail.com>
2021-12-12 07:22:25 +0800

    Remove redundant verifyToken from apistack

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -32,20 +32,11 @@ export default class ApiStack extends sst.Stack {
       },
     })
 
-    const verifyToken = new sst.Function(this, 'VerifyToken', {
-      handler: 'src/lambda/verifyToken.handler',
-      environment: {
-        GRAPHQL_URL: process.env.GRAPHQL_URL,
-        HASURA_SECRET: process.env.HASURA_SECRET,
-      },
-    })
-
     // Create a HTTP API
     this.api = new sst.Api(this, 'Api', {
       routes: {
         'GET /v1/compute-expense-report': computeExpenseReport,
         'POST /v1/delete-s3-image': deleteS3Image,
-        'GET /v1/verify-token': verifyToken,
       },
     })
 
@@ -53,7 +44,6 @@ export default class ApiStack extends sst.Stack {
     this.addOutputs({
       computeExpenseReport: `GET ${this.api.url}/v1/compute-expense-report`,
       deleteS3Image: `POST ${this.api.url}/v1/delete-s3-image`,
-      verifyToken: `GET ${this.api.url}/v1/verify-token`,
     })
   }
 }
